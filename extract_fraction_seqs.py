5ae89b14998392287ffee5156a1e346bde1abf48
Ginsea <ginseachen@hotmail.com>
2016-03-16 16:25:29 +0800

    Create extract_fraction_seqs.py

---
+++ extract_fraction_seqs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+@Author This script is developed by Ginsea Chen (ginseachen@hotmail.com) of CATAS
+@Descriptions You can use this script to exrtact some sequences from a fasta and then produce a new fasta file
+@Infile The file with fasta formati
+@Outfile The file with fasta format
+@numbers How many sequences you want to exract
+'''
+
+import sys
+import os, os.path
+from Bio import SeqIO
+
+if sys.argv[1] == "-h":
+    print "Usage:\n\tpython %s infile outfile numbers"%os.path.basename(__file__)
+    sys.exit(0)
+
+out = open(sys.argv[2],"w")
+ 
+y = sys.argv[3]
+
+ids = []
+fasta = dict()
+for seqs in SeqIO.parse(sys.argv[1],"fasta"):
+    ids.append(str(seqs.id))
+    fasta[str(seqs.id)] = str(seqs.seq)
+
+for sids in ids[:int(y)]:
+    out.write(">%s\n%s\n"%(sids, fasta[sids]))
+out.close()
